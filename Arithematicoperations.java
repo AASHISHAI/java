c6ade223433ec74c3766d0e93b135abba9469ae7
Aashish Kumar Jaiswal <121435379+AASHISHAI@users.noreply.github.com>
2023-08-29 12:09:24 +0530

    Add files via upload

---
+++ Arithematicoperations.java
@@ -0,0 +1,45 @@
+class AddSub
+{
+   int n1,n2;
+   public AddSub(int x,int y)
+      {
+          n1=x;
+          n2=y;
+       }
+      public int add()
+           {
+                  return(n1+n2);
+            }
+       public  int sub()
+           {
+                return(n1-n2);
+           }
+}
+   class MulDiv extends AddSub
+       {
+          public MulDiv(int x,int y)
+             {
+                 super(x,y);
+             }
+           public int mul()
+              {
+                  return(n1*n2);
+              }
+           public int div()
+               {
+                   return(n1/n2);
+               }
+           }
+                public class Arithematicoperations
+          {
+               public static void main(String args[])
+                   {
+                      MulDiv ob=new MulDiv(20,10);
+                      System.out.println("Sum="+ob.add());
+                      System.out.println("Difference="+ob.sub());
+                      System.out.println("Product="+ob.mul());
+                      System.out.println("Division="+ob.div());
+                   }
+             
+              
+}
