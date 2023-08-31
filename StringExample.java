0e64b476c8af92d40e82de001f817748648a4a2c
Aashish Kumar Jaiswal <121435379+AASHISHAI@users.noreply.github.com>
2023-08-31 22:54:58 +0530

    Add files via upload

---
+++ StringExample.java
@@ -0,0 +1,18 @@
+public class StringExample {
+    public static void main(String args[]){
+        String str="we are here to learn java";
+        String str1=" ";
+        char[] ch={'a','e','i','o','u'};
+        for(char c:ch)
+        {
+            for(int i=0;i<str.length();i++){
+                if(str.charAt(i)==c){
+                    str1 +=c;
+
+                }
+            }
+        }
+        System.out.println(str1);
+    }
+    
+}
