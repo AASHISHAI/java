88f4155eab7def236640831496f6faf011c42295
Aashish Kumar Jaiswal <121435379+AASHISHAI@users.noreply.github.com>
2023-09-01 23:43:13 +0530

    Add files via upload

---
+++ NPE.java
@@ -0,0 +1,15 @@
+public class NASE
+{
+public static void main(String args[])
+{
+try
+{
+int [] array=new int[-1];
+}
+Catch(NegativeArraySizeException obj)
+{
+Obj.printStackTrace();
+}
+System.out.println(“Exception caught and continue execution”);
+}
+}
