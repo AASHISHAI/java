6bdacfb8a03ee976ff57c71061d3e7675a4e3cc1
Aashish Kumar Jaiswal <121435379+AASHISHAI@users.noreply.github.com>
2023-09-03 23:54:34 +0530

    Add files via upload

---
+++ AreaOfSquare.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class AreaOfSquare {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int a;
+        System.out.println("Enter the value of a :");
+        a=sc.nextInt();
+        int area=(a*a);
+        System.out.println("The area of square :"+area);
+
+    }
+    
+}
