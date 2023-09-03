d238e338829530731ce92bd6ed0cbffbe7513d4d
Aashish Kumar Jaiswal <121435379+AASHISHAI@users.noreply.github.com>
2023-09-03 23:54:14 +0530

    Add files via upload

---
+++ AreaofSphere.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class AreaofSphere {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the value of r :");
+        float r=sc.nextInt();
+        double area=((4*3.14*r*r));
+        System.out.println("The area of Sphere is :"+area);
+    }
+    
+}
