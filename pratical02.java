a115a05a9dfa7921f89491e8d234fc494fdb8529
Aashish Kumar Jaiswal <121435379+AASHISHAI@users.noreply.github.com>
2023-10-18 20:36:25 +0530

    Add files via upload

---
+++ pratical02.java
@@ -0,0 +1,12 @@
+// Datedemo Program
+import java.util.*;
+public class pratical02 {
+    public static void main(String args[]){
+        Calendar cal= Calendar.getInstance();
+        String[] month={"January","February","March","April","May","June","July","August","September","October","November","December"};
+        System.out.println("Current month="+month[cal.get(Calendar.MONTH)]);
+
+
+    }
+    
+}
