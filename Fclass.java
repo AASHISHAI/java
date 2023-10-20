6424d7f976447411a99cb88afd409b3c21179b90
Aashish Kumar Jaiswal <121435379+AASHISHAI@users.noreply.github.com>
2023-10-20 21:35:34 +0530

    Add files via upload

---
+++ Fclass.java
@@ -0,0 +1,12 @@
+public class Fclass {
+    public static void main(String args[]){
+        int i=10;
+        int j=29;
+        double d;
+        d= i/j*j;
+        System.out.println(d+" ");
+        d=j*i/j;
+        System.out.println(d+" ");
+    }
+    
+}
